ade1e400dc473bdd3212b3d12b9a479c05bc58d3
MissTipo <dorine.a.tipo@gmail.com>
2022-10-11 22:40:42 +0300

    fix betty

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -17,12 +17,12 @@ void bubble_sort(int *array, size_t size)
 	for (i = 0; i < size; i++)
 	{
 		swap = 0;
-		for (j = 0; j < size -i-1; j++)
+		for (j = 0; j < size - i - 1; j++)
 		{
-			if (array[j] > array[j+1])
+			if (array[j] > array[j + 1])
 			{
-				temp = array[j+1];
-				array[j+1] = array[j];
+				temp = array[j + 1];
+				array[j + 1] = array[j];
 				array[j] = temp;
 
 				swap = 1;
